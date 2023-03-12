6dd4cb32af902585fed3a899859230b2411d8479
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-03-12 14:56:26 +0300

    Output func

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна трём символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не реккомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] MassivStrok;
+string[] newMassivStrok;
+System.Console.Write("Введите исходную строку: ");
+string inputStroka = Console.ReadLine();
+MassivStrok = inputStroka.Split(' ');
+output(newMassivStrok = newMassiv(MassivStrok));
+
+void output(string[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        System.Console.WriteLine(Arr[i]);
+    }
+}
+
+
+
